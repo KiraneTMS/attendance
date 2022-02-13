8dca8ea5cf60271d2203fee8f395db99563cc37a
KiraneTMS <kirenaiasakura@gmail.com>
2022-02-13 21:32:50 +0700

    register layout, some system

--- app/src/main/java/com/example/attendance/SignIn.java
+++ app/src/main/java/com/example/attendance/SignIn.java
@@ -3,12 +3,20 @@ package com.example.attendance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signin);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
