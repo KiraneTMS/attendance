8407eeeb3ef723b39cbc888bbb8b7a4c8babf1e5
KiraneTMS <kirenaiasakura@gmail.com>
2022-02-17 12:15:58 +0700

    sliding Navbar

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -1,31 +1,126 @@
 package com.example.attendance;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
+
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener{
 
     Button logoutBtn;
     FirebaseAuth fAuth;
+
+    private static final int POS_CLOSE = 0;
+    private static final int POS_DASHBOARD = 1;
+    private static final int POS_ABOUT_US = 2;
+    private static final int POS_LOGOUT = 3;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slidingRootNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logoutBtn = findViewById(R.id.logoutBtn);
+//        logoutBtn = findViewById(R.id.logoutBtn);
         fAuth = FirebaseAuth.getInstance();
 
 //        logoutBtn.setOnClickListener(view -> {
 //            logout();
 //        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withDragDistance(180)
+                .withRootViewScale(0.75f)
+                .withRootViewElevation(25)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.drawer_menu)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_CLOSE),
+                createItemFor(POS_DASHBOARD).setChecked(true),
+                createItemFor(POS_ABOUT_US),
+                new SpaceItem(260),
+                createItemFor(POS_LOGOUT)
+        ));
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.drawerList);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_DASHBOARD);
+
+    }
+
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.black))
+                .withTextTint(color(R.color.light_blue))
+                .withSelectedIconTint(color(R.color.black))
+                .withSelectedTextTint(color(R.color.black));
+    }
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.id_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.id_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++){
+            int id = ta.getResourceId(i, 0);
+
+            if (id!=0){
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res){
+        return ContextCompat.getColor(this, res);
     }
 
     private void logout() {
@@ -40,5 +135,24 @@ public class MainActivity extends AppCompatActivity {
         if (currentUser == null){
             startActivity(new Intent(MainActivity.this, Register.class));
         }
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (position == POS_DASHBOARD){
+            DashboardFragment fragment = new DashboardFragment();
+            transaction.replace(R.id.container, fragment);
+        }else if (position == POS_ABOUT_US){
+            AboutUsFragment fragment = new AboutUsFragment();
+            transaction.replace(R.id.container, fragment);
+        }else if (position == POS_LOGOUT){
+            finish();
+        }
+
+        slidingRootNav.closeMenu();
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
