55efdb1ff6c7df9c86107b91774af9da82b6f1a6
KiraneTMS <kirenaiasakura@gmail.com>
2022-02-27 09:02:08 +0700

    sliding Navbar

---
+++ app/src/main/java/com/example/attendance/Users.java
@@ -0,0 +1,32 @@
+package com.example.attendance;
+
+public class Users {
+
+    private String username;
+    private String phone;
+    private String email;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
